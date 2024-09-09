893f5cd1bb4c1e2318c6c4d57bb336e215f16272
gilhan <rlfgks97@gmail.com>
2024-09-09 16:05:28 +0900

    about Ranges

---
+++ hello-world/src/main/kotlin/Ranges.kt
@@ -0,0 +1,14 @@
+package com.varchar6.petcast
+
+fun main() {
+    for (char in 'a'..'x' step 10) {
+        println(char)
+    }
+
+    for (nums in 5..15) {
+        println(nums)
+    }
+
+    val range = 1..10 step 2  // 1, 3, 5, 7, 9
+
+}
