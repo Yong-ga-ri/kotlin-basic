2e1df09131d24dc9ad58f61606188f5699b29ab4
gilhan <rlfgks97@gmail.com>
2024-09-09 15:25:27 +0900

    about IfElse

---
+++ hello-world/src/main/kotlin/IfElse.kt
@@ -0,0 +1,11 @@
+package com.varchar6.petcast
+
+fun main() {
+    val time = 20
+    val greeting = if (time < 18) {
+        "Good day."
+    } else {        // 필기. 반환값이 존재하는 표현식이기에, 반드시 else문이 필요
+        "Good evening."
+    }
+    println(greeting) // 필기. java의 삼중 조건문과 동일
+}
